a0c27c80f4bdf97968ea2bd8c2cf8f808c262805
ShannaLiu <97636422+ShannaLiu@users.noreply.github.com>
2022-11-06 20:35:34 -0600

    add constraint method with lap loss

--- Selection/constraint/op_const_lap.py
+++ Selection/constraint/op_const_lap.py
@@ -1,6 +1,8 @@
+# A constraint method for optimizing the Laplacian loss
+
 import torch
 
-def op_const(X, T, r, epsilon=1e-3, ratio=0.5):
+def op_const_lap(X, L, r, epsilon=1e-3, ratio=0.5):
     N = X.shape[0]
     W = torch.zeros(N,N)
     W_tilde = torch.zeros(N,N)
@@ -10,18 +12,18 @@ def op_const(X, T, r, epsilon=1e-3, ratio=0.5):
 
     while error.item() > epsilon:
         W_tilde = W 
-        W = update_W(W_tilde, X, lamda, T, r)
+        W = update_W(W_tilde, X, lamda, L, r)
         error = torch.max(torch.abs(W-W_tilde))
-        loss_rec.append(compute_loss(W, T, X))
+        loss_rec.append(compute_loss(W, L, X))
+        print(error)
     return W, torch.tensor(loss_rec)
 
 
-def update_W(W_tilde, X, lamda, T, r):
+def update_W(W_tilde, X, lamda, L, r):
     N = X.shape[0]
     A_tilde = W_tilde - lamda * 2 * torch.matmul((W_tilde-torch.diag(torch.ones(N))), torch.matmul(X, X.t()))
-    W_star = (A_tilde > lamda*torch.abs(T)).float() * (A_tilde - lamda*torch.abs(T)) + \
-        (A_tilde < -lamda*torch.abs(T)).float() * (A_tilde + lamda*torch.abs(T))
-    column_loss = 1/2*torch.sum((W_star-A_tilde).pow(2), dim=0) + lamda * torch.sum(torch.abs(T*W_star), dim=0)
+    W_star = torch.linalg.solve(2*L*lamda+torch.diag(torch.ones(N)), A_tilde)
+    column_loss = 1/2*torch.sum((W_star-A_tilde).pow(2), dim=0) + lamda * torch.diag(torch.matmul(W_star.t(), torch.matmul(L, W_star)))
     ind = torch.sort(column_loss, descending=True).indices
     W = torch.zeros_like(W_star)
     W[:,ind[:r]] = W_star[:, ind[:r]]
