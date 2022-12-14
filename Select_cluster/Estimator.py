33f22c6a1b50571e403072b555cc20e71580e4fc
ShannaLiu <97636422+ShannaLiu@users.noreply.github.com>
2022-12-14 16:14:12 -0600

    change solver

---
+++ Select_cluster/Estimator.py
@@ -0,0 +1,4 @@
+import numpy as np
+import cvxpy as cp
+from sklearn.base import BaseEstimator
+from sklearn.utils.validation import check_is_fitted
--- Select_cluster/solver.py
+++ Select_cluster/solver.py
@@ -2,19 +2,20 @@ import numpy as np
 import cvxpy as cp
 from scipy.linalg import solve_sylvester
 from loss import *
+from scs import SCS
 
 def elast_cp_solver(X, Gamma, l1, l2, maxiter):
     n = X.shape[0]
     W = cp.Variable((n,n))
     prob = cp.Problem(cp.Minimize( recon_loss(X, W) + ee_penalty(Gamma, W, l1, l2) ))
-    prob.solve(max_iters=maxiter)
+    prob.solve(max_iters=maxiter, solver='SCS')
     return W.value
 
 def sparsel2_cp_solver(X, Gamma, l1, l2, maxiter):
     n = X.shape[0]
     W = cp.Variable((n,n))
     prob = cp.Problem(cp.Minimize( recon_loss(X, W) + sparse_lap(Gamma, W, l1, l2) ))
-    prob.solve(max_iters=maxiter)
+    prob.solve(max_iters=maxiter, solver='SCS')
     return W.value
 
 def lap_sylvester_solver(X, L, l2):
